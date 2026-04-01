6184a469ebabc6f68a99eee1c3a8c7104b126f44
jayant <potnurujayant445@gmail.com>
2026-04-01 19:46:52 +0530

    Update nlp.py

--- app/services/nlp.py
+++ app/services/nlp.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer
+from app.config.settings import all-MiniLM-L6-v2
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_embedding(text: str):
     return model.encode(text)
