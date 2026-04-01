cde9a72ef0cffd13ca2ffc84c0f60fe6b1b97003
jayant <potnurujayant445@gmail.com>
2026-04-01 19:44:39 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.recommend import router
 
 app = FastAPI(title="Influencer Recommendation API")
+
+# CORS (important for frontend)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
